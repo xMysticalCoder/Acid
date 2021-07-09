b568293beabcc9424b0165494ecaabfca981dd2f
xMysticalCoder <74260385+xMysticalCoder@users.noreply.github.com>
2021-07-09 17:24:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#Connected to github :D
+
+
+
+print("Acid Version 1.01")
+variables = dict()
+def readcode():
+  if code.startswith("p(\"") and code.endswith("\")"):
+    print(code[3:-2])
+  elif code.startswith("v "):
+    r = code.split()
+    variables[r[1]] = r[-1]
+    print(variables)
+  elif code.startswith("p(") and code.endswith(")"):
+    e = code[2:-1]
+    try:
+      print(variables[e])
+    except:
+      print("No variable \"", e, "\" found")
+
+    
+  else:
+    print("Syntax not understood.")
+
+while True:
+  code = input(">> ")
+  readcode()
+  
